7d3c37a1733a8943fc35a0343c433e258eb86d9e
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2021-07-31 23:03:32 +0600

    Added state in the Login component, styled it, added a bit more logic to have the form render differently based on state(more logic to come) and finally rendered the Login component in the App.js page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    /*In a real app this is where the password would be further checked but since this is a dummy app we are straight away going to changing the state.*/
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <React.Fragment>
-      <MainHeader />
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
-        <Login />
+        <Login onLogin={loginHandler} />
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
     </React.Fragment>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Login.module.css";
+import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
 
-const Login = () => {
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
   return (
-    <form>
-      <div>
-        <label htmlFor="email">E-Mail</label>
-        <input type="email" id="email" />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" id="password" />
-      </div>
-      <div></div>
-    </form>
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" value={enteredEmail} />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" id="password" value={enteredPassword} />
+        </div>
+        <div>
+          <Button>Login</Button>
+        </div>
+      </form>
+    </Card>
   );
 };
 
